5677792978b91a7ddfbf4f4ec18ec859a3dc9e06
Mrcr1 <ramrchopade@gmail.com>
2026-05-03 12:46:57 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -7,6 +8,16 @@ int main(int argc, char *argv[]) {
 
   // TODO: Uncomment the code below to pass the first stage
    printf("$ ");
+
+   //Captures User's command in Command Variable;
+  char command[1024];
+  fgets(command, sizeof(command), stdin);
+  
+  //Remove Trailing Newline
+  command[strcspn(command, "\n")] = '\0';
+
+  // Prints the "<command>: command not found" message
+  printf("%s: command not found\n", command);
 
   return 0;
 }
