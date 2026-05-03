96cea1392433013b956dec094690dd0b7a4bafbe
Mrcr1 <ramrchopade@gmail.com>
2026-05-03 18:27:49 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]) {
       break;
     }
 
-    else if (strcmp(command, "echo") == 0)
+    else if (strcmp(command, "echo", 5) == 0)
     {
       printf("%s\n", command + 5);
     }
