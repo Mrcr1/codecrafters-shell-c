34a31ba14be946bcc6aaef9ee6518588e49322cd
Mrcr1 <ramrchopade@gmail.com>
2026-05-08 12:41:30 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
@@ -10,33 +11,40 @@ int main(int argc, char *argv[])
     {
         printf("$ ");
 
+        // Read the entire command line input
         char command[1024];
         fgets(command, sizeof(command), stdin);
         command[strcspn(command, "\n")] = '\0';
 
-        // Split on the first space only
+        // Split on the first space only to separate the builtin command from its argument
         char *space = strchr(command, ' ');
         char *builtin = command;
         char *arg = NULL;
 
+        // If there is a space, terminate the builtin command and set arg to the rest of the string
         if (space != NULL)
         {
             *space = '\0';   // terminate builtin name
             arg = space + 1; // arg = everything after the first space
         }
 
-        // Skip empty input
+        // Builtin Block Starts
         if (*builtin == '\0')
             continue;
 
+        // Exit builtin:
         if (strcmp(builtin, "exit") == 0)
         {
             break;
         }
+
+        // Echo builtin:
         else if (strcmp(builtin, "echo") == 0)
         {
             printf("%s\n", arg ? arg : "");
         }
+        
+        // type builtin:
         else if (strcmp(builtin, "type") == 0)
         {
             if (arg == NULL)
@@ -47,9 +55,43 @@ int main(int argc, char *argv[])
             {
                 printf("%s is a shell builtin\n", arg);
             }
+                        
+            // type executables:
             else
             {
-                printf("%s: not found\n", arg);
+                char *path_env = getenv("PATH");
+                int found = 0;
+                
+                if (path_env != NULL)
+                {
+                    //Duplicate the PATH environment variable to avoid modifying the original
+                    char *path_copy = strdup(path_env);
+                    char *dir = strtok(path_copy, ":");
+
+                    while (dir != NULL)
+                    {
+                        char full_path[1024];
+                        snprintf(full_path, sizeof(full_path), "%s/%s", dir, arg);
+
+                        if (access(full_path, X_OK) == 0)
+                        {
+                            printf("%s is %s\n", arg, full_path);
+                            found = 1;
+                            break;
+                        }
+
+                        dir = strtok(NULL, ":");
+                    }
+
+                    free(path_copy);
+                    
+                }
+
+                if (!found)
+                {
+                    printf("%s not found\n", arg);
+                }
+                
             }
         }
         else
